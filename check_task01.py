248fd23d2e557b6f98c52058a334cce00dbe7656
Gleb Dovzhenko <dovjenko.g@gmail.com>
2024-01-09 12:54:38 +0700

    Added docstrings

--- check_task01.py
+++ check_task01.py
@@ -3,9 +3,19 @@ import numpy as np
 
 
 class TestTask01(TestStudentReport):
+    """
+    This is an example of how the student reports can be checked.
+    
+    File task01.ipynb contains some cells within which are 
+    3 variable definitions: a, b, c.
+    This test case compares the values of these variables against some hard-coded values.
+    """
     _report_name = 'task01'
     
     def test_a(self):
+        """
+        a = np.array([1, 2, 3])
+        """
         self.assertTrue(
             np.all(np.isclose(
                 self.report['a'],
@@ -14,11 +24,17 @@ class TestTask01(TestStudentReport):
         )
 
     def test_b(self):
+        """
+        b = True
+        """
         self.assertTrue(
             self.report['b']
         )
     
     def test_c(self):
+        """
+        c = {1: 2, 3: 4}
+        """
         self.assertDictEqual(
             self.report['c'],
             {1: 2, 3: 4}
