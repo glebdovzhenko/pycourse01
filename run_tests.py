8580956feae5998663578521b4b3b1fad6a9065d
Gleb Dovzhenko <dovjenko.g@gmail.com>
2024-01-10 11:46:15 +0700

    Moving to another testrunner

--- run_tests.py
+++ run_tests.py
@@ -1,14 +1,13 @@
 import answers
-import unittest
+from unittest import TextTestRunner, TextTestResult, TestLoader
 from HtmlTestRunner import HTMLTestRunner
 
 
 if __name__ == '__main__':
-    runner = HTMLTestRunner(
-        template='test_engine/report_template.html',
-        combine_reports=True
-    )
-    loader = unittest.TestLoader()
-    suite = loader.loadTestsFromModule(answers)
-    print(suite)
+    # runner = HTMLTestRunner(
+    #     template='test_engine/report_template.html',
+    #     combine_reports=True
+    # )
+    runner = TextTestRunner(resultclass=TextTestResult)
+    suite = TestLoader().loadTestsFromModule(answers)
     runner.run(suite)
