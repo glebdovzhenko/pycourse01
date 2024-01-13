da2d337a16a6c15b24fe997759b266f38403e5a6
Gleb Dovzhenko <dovjenko.g@gmail.com>
2024-01-13 18:13:01 +0700

    updated report template

--- test_engine/utest.py
+++ test_engine/utest.py
@@ -7,30 +7,49 @@ from .jup import NotebookFinder
 from jinja2 import Template
 
 
-
 def setUpModule():
     sys.meta_path.append(NotebookFinder())
 
 
 class TestStudentReport(unittest.TestCase):
     _report_name = ''
+    _varlist = ()
 
-    def __init__(self, 
+    def __init__(self,
                  methodName: str = "runTest") -> None:
         super().__init__(methodName)
+        self.longMessage = False
 
     @classmethod
     def setUpClass(cls):
         cls._report = importlib.import_module(cls._report_name)
-    
+
     @property
     def report(self):
         return self._report.__dict__
 
+    # strange name so that it always runs first
+    def test0_variables(self):
+        """
+        Определены все переменные.
+        """
+        not_in_list = []
+        for v_name in self._varlist:
+            if v_name not in self.report:
+                not_in_list.append(v_name)
+
+        if not_in_list:
+            self.assertIn(
+                not_in_list[0], self.report,
+                msg='Переменные %s не определены' % ', '.join(not_in_list)
+            )
+
 
 DEFAULT_TEMPLATE = os.path.join(
-    os.path.dirname(__file__), 
-    "report_template_2.html"
+    os.path.dirname(__file__),
+    '..',
+    'templates',
+    'report_template_2.html'
 )
 
 
@@ -62,36 +81,43 @@ class JinjaTestResult(unittest.TestResult):
 
         self._load_template()
 
-    def addSuccess(self, test) -> None:
-        cls_key = test.__doc__
-        case_key = test._testMethodDoc
+    def add_test_result(self, test, result, msg='') -> None:
+        cls_key = test.__doc__.replace('\n', '')
+        case_key = test._testMethodDoc.replace('\n', '')
         if cls_key not in self._jinja_context['results'].keys():
             self._jinja_context['results'][cls_key] = dict()
-            self._jinja_context['summaries'] = ''
-        self._jinja_context['results'][cls_key][case_key] = self.SUCCESS
+            self._jinja_context['summaries'][cls_key] = ''
+        self._jinja_context['results'][cls_key][case_key] = (result, msg)
+
+    def addSuccess(self, test) -> None:
+        self.add_test_result(test, self.SUCCESS)
         return super().addSuccess(test)
 
     def addFailure(self, test: unittest.case.TestCase, err) -> None:
-        cls_key = test.__doc__
-        case_key = test._testMethodDoc
-        if cls_key not in self._jinja_context['results'].keys():
-            self._jinja_context['results'][cls_key] = dict()
-            self._jinja_context['summaries'] = ''
-        self._jinja_context['results'][cls_key][case_key] = self.FAILURE
-
+        self.add_test_result(test, self.FAILURE, err[1])
         return super().addFailure(test, err)
 
-    def addSubTest(self, test, subtest, err) -> None:
-        print(test.__doc__)
-        print(subtest.__doc__)
-        return super().addSubTest(test, subtest, err)
+    def addError(self, test, err) -> None:
+        self.add_test_result(test, self.ERROR, err[1])
+        return super().addError(test, err)
+
+    def addSkip(self, test, reason) -> None:
+        self.add_test_result(test, self.SKIP)
+        return super().addSkip(test, reason)
 
     def generate_report(self, extra_context=dict(), report=None):
+        for cls_key in self._jinja_context['results']:
+            self._jinja_context['summaries'][cls_key] = all(
+                (cs[0] == self.SUCCESS) for (_, cs) in self._jinja_context['results'][cls_key].items()
+            )
+
         if report is None:
             with open(DEFAULT_REPORT, 'w') as f:
                 f.write(
                     self._jinja_template.render(
-                        **self._jinja_context, **extra_context)
+                        **self._jinja_context,
+                        **extra_context
+                    )
                 )
         else:
             raise NotImplementedError()
@@ -103,10 +129,11 @@ class JinjaTestResult(unittest.TestResult):
             )
         else:
             raise NotImplementedError()
-            
+
 
 class JinjaTestRunner(unittest.TextTestRunner):
     resultclass = JinjaTestResult
+
     def __init__(self, **kwargs) -> None:
         self.extra_context = kwargs.pop('extra_context', dict())
         super().__init__(**kwargs)
@@ -118,6 +145,3 @@ class JinjaTestRunner(unittest.TextTestRunner):
                 extra_context=self.extra_context
             )
         return result
-
-
-
