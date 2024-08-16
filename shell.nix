08287ae8c74df8ec2affc10cf920450f124f4bd0
Gleb Dovzhenko <dovjenko.g@gmail.com>
2024-08-16 14:26:05 +0700

    Update btw semesters

---
+++ shell.nix
@@ -0,0 +1,22 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  nativeBuildInputs = [ pkgs.qt5.qttools.dev ];
+
+  propagatedBuildInputs = [
+    (pkgs.python3.withPackages (ps: with ps; [
+      matplotlib
+      pyqt5
+      setuptools
+      numpy
+      scipy
+      pandas
+      jupyterlab
+    ]))
+
+  ];
+
+  # Normally set by the wrapper, but we can't use it in nix-shell (?).
+  QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins";
+}
+
